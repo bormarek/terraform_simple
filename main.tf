25b88ab690716ccbbc658aeed2cd58f81cde75a9
Marek Borkowski <marek@MacBook-Air.lan>
2024-02-03 13:58:45 +0100

    changes

--- main.tf
+++ main.tf
@@ -4,3 +4,7 @@ resource "local_file" "example" {
   content  = "To jest zawartość pliku tekstowego, stworzonego przez Terraform."
   filename = "example1.txt"
 }
+resource "local_file" "example1" {
+  content = "Test file"
+  filename = "example2.txt"
+}
